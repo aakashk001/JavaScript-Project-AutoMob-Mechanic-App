2e996d8de75ed5606ac1502cea7397ec289ddbab
aakashk001 <81735145+aakashk001@users.noreply.github.com>
2022-06-08 16:20:19 +0530

    Add files via upload

---
+++ automobmechanic/js/main.js
@@ -0,0 +1,144 @@
+function redirectToServices() {
+    window.location.assign("services.html");
+}
+
+function validateForm() {
+    var username = document.getElementById("uname").value;
+    console.log(username);
+    var password = document.getElementById("pwd").value;
+    console.log(password);
+
+    fetch('../js/users.json').then(result => result.json()).then(json => {
+        let flag = false;
+        for (let i = 0; i < 6; i++) {
+            if (username == json[i].username && password == json[i].password) {
+                alert("Logged Sucessfully");
+                flag = true;
+                flag1 = true;
+                sessionStorage.setItem("UserName", username);
+                sessionStorage.setItem("Flag", "true");
+                window.location.assign("home.html")
+                break;
+            }
+        }
+        if (flag == false) {
+            alert("Invalid Credentails");
+        }
+    })
+}
+
+if(sessionStorage.getItem("Flag") == "true") {
+    changeNavBar();
+}
+
+function changeNavBar() {
+    
+    if(sessionStorage.getItem("UserName") == "admin"){
+        document.getElementById('login').style.display = "none";
+        document.getElementById("services").style.display = "none";
+        document.getElementById("booking").style.display = "none";
+        sessionStorage.removeItem("flag");
+        let parent = document.getElementById("name");
+         parent.textContent = 'Hi ' + sessionStorage.getItem("UserName") + " |";
+     
+    }
+    else {
+   document.getElementById("login").style.display = "none";
+  document.getElementById('reports').style.display = "none";
+  sessionStorage.removeItem("flag");
+  let parent = document.getElementById("name");
+    parent.textContent = 'Hi ' + sessionStorage.getItem("UserName") + " |";
+    }
+}
+//function used in services.html
+function MoreDetailsPMS(){
+sessionStorage.setItem("value" ,"1");
+window.location.assign("preventive-maintenance-service.html");
+
+
+}
+
+function MoreDetailsBPS(){
+    sessionStorage.setItem("value" ,"2");
+    window.location.assign("body-repair-service.html");
+
+    }
+function MoreDetailsCCS(){
+    sessionStorage.setItem("value" ,"3");
+    window.location.assign("car-care-service.html");
+    }
+
+
+function countDown(){
+
+}
+
+//function PMS
+function bookingPMS(){
+    sessionStorage.setItem("Stage1","true")
+    window.location.assign("booking.html"); //REDIRECT TO booking.html
+}
+// so that the selectservice will have the default value according the servies you choose
+
+//redirect the booking.html
+function bookingBPS(){
+    sessionStorage.setItem("Stage1","true")
+    window.location.assign("booking.html");
+    
+}
+
+function bookingCCS(){
+    sessionStorage.setItem("Stage1","true")
+    window.location.assign("booking.html");
+ 
+}
+//redirect back to the last page opended
+function backUrl(){
+  window.history.back();
+}
+
+if( sessionStorage.getItem("Stage1")=="true"){
+    document.getElementById("selectService").selectedIndex = sessionStorage.getItem("value"); 
+    sessionStorage.removeItem("Stage1");
+  //  sessionStorage.removeItem("value");
+}
+
+
+
+
+function validatebooking(){
+    sessionStorage.setItem("bookingsuccess","true");
+    checkDate();
+ return true;
+}
+
+document.getElementById("appointmentDate").addEventListener("change", function() {
+     var input = this.value;
+        console.log(input); //e.g. 2015-11-13
+        let today = new Date().toISOString().slice(0, 10)
+        console.log(today);   
+        if(input < today ){
+            document.getElementById("errorMsg").innerHTML = "*Error Please entre a Valid date"
+                console.log("Error Please entre a valid date");
+                }
+            else{
+                document.getElementById("errorMsg").innerHTML = "";
+            } });
+
+
+if(sessionStorage.getItem("bookingsuccess") == "true"){
+    setTimeout(()=>{
+        console.log("You are insde this function")
+        prompt("On a scale of 1-10, how likely are you to recommend our website to your friends or family?");
+        alert("Thanks for your feedback");
+        sessionStorage.removeItem("bookingsuccess");
+        ;},6000);
+}
+
+
+
+function logout(){
+    sessionStorage.clear();
+    alert("Logged out successfully");
+    window.location.assign("home.html");
+}
